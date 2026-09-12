3ff7912bf0cf1d10efeb0c3f81b28054ddc0c6d8
D. Tunegov <d.tunegov@yandex.ru>
2026-09-12 11:57:05 +0300

    Добавил таблицы 2 и 3, отредактировал файлы, дописал программу

--- PM_1623_Tunegov/Program.cs
+++ PM_1623_Tunegov/Program.cs
@@ -6,17 +6,52 @@ namespace PM_1623_Tunegov
     {
         static void Main()
         {
-            // Исходные данные Таблицы 1
-            int[] supply = { 20, 45, 24, 31, 30 };
-            int[] demand = { 65, 44, 41 };
-            int[,] cost = {
+
+            Console.WriteLine("РАСЧЕТ ТРАНСПОРТНОЙ ЗАДАЧИ ДЛЯ ТАБЛИЦ 1, 2 И 3");
+
+            // --- ТАБЛИЦА 1
+            int[] supply1 = { 20, 45, 24, 31, 30 };
+            int[] demand1 = { 65, 44, 41 };
+            int[,] cost1 = {
                 { 5, 4, 6 },
                 { 7, 3, 3 },
                 { 9, 5, 2 },
                 { 3, 2, 5 },
                 { 4, 7, 1 }
             };
+            SolveTable("ТАБЛИЦА 1", supply1, demand1, cost1);
 
+            Console.WriteLine("\n--------------------------------------------------\n");
+
+            // --- ТАБЛИЦА 2
+            int[] supply2 = { 40, 50, 30, 60 };
+            int[] demand2 = { 70, 50, 60 };
+            int[,] cost2 = {
+                { 4, 2, 5 },
+                { 3, 6, 2 },
+                { 5, 1, 4 },
+                { 2, 4, 3 }
+            };
+            SolveTable("ТАБЛИЦА 2", supply2, demand2, cost2);
+
+            Console.WriteLine("\n--------------------------------------------------\n");
+
+            // --- ТАБЛИЦА 3
+            int[] supply3 = { 60, 80, 50 };
+            int[] demand3 = { 40, 60, 50, 40 };
+            int[,] cost3 = {
+                { 6, 3, 2, 5 },
+                { 4, 5, 1, 3 },
+                { 2, 7, 4, 6 }
+            };
+            SolveTable("ТАБЛИЦА 3", supply3, demand3, cost3);
+
+            Console.WriteLine("\nНажмите Enter для выхода...");
+            Console.ReadLine();
+        }
+
+        static void SolveTable(string title, int[] supply, int[] demand, int[,] cost)
+        {
             int m = supply.Length;
             int n = demand.Length;
             int[,] plan = new int[m, n];
@@ -24,7 +59,7 @@ namespace PM_1623_Tunegov
             int[] curSupply = (int[])supply.Clone();
             int[] curDemand = (int[])demand.Clone();
 
-            // Расчет методом минимальных элементов
+            // Поиск ячеек с минимальным тарифом
             while (true)
             {
                 int minCost = int.MaxValue;
@@ -55,7 +90,9 @@ namespace PM_1623_Tunegov
 
             // Вывод результатов
             int totalCost = 0;
-            Console.WriteLine("Опорный план для Таблицы 1");
+            Console.WriteLine($"--- {title} ---");
+            Console.WriteLine("Матрица опорного плана:");
+
             for (int i = 0; i < m; i++)
             {
                 for (int j = 0; j < n; j++)
@@ -67,7 +104,6 @@ namespace PM_1623_Tunegov
             }
 
             Console.WriteLine($"\nИтоговая стоимость грузоперевозки = {totalCost}");
-            Console.ReadLine();
         }
     }
 }
